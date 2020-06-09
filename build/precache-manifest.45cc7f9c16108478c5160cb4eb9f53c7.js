e19e957afd55c28c5d4760262a09919c66e766ab
avichalkesharwani <kesharwani.avichal@gmail.com>
2020-06-09 11:54:46 +0530

    initial

---
+++ build/precache-manifest.45cc7f9c16108478c5160cb4eb9f53c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a261b2ca5251139ee570dbb8c375a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7001b1bd5c53a70ecd9e",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "c4647087df6bc8dfa0cd",
+    "url": "/static/css/main.09924d87.chunk.css"
+  },
+  {
+    "revision": "7001b1bd5c53a70ecd9e",
+    "url": "/static/js/2.e4484673.chunk.js"
+  },
+  {
+    "revision": "c4647087df6bc8dfa0cd",
+    "url": "/static/js/main.74390539.chunk.js"
+  },
+  {
+    "revision": "808f9ecbf4c51e05d1f7",
+    "url": "/static/js/runtime-main.befe6cb0.js"
+  }
+]);
